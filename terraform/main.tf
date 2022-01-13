b6da4de9e65d49b21b5e86b35df54308e1f78368
juniorcfaj <46963611+juniorcfaj@users.noreply.github.com>
2022-01-13 13:41:37 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ provider "azurerm" {
   features {}
 }
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.50.0"
+    }
+  }
   backend "azurerm" {
     storage_account_name = "tstate14754"
     container_name       = "tstate"
