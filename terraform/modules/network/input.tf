67f43c95fbe3e28ca0bca2d5d87cfd7fbb07397b
juniorcfaj <junior.cfaj@gmail.com>
2022-01-16 00:41:15 -0300

    vai

--- terraform/modules/network/input.tf
+++ terraform/modules/network/input.tf
@@ -1,24 +1,9 @@
 # Resource Group
-variable resource_group {
-    default = "RG-Project-qa"
-}
-variable location {
-    default = "eastus2"
-}
+variable resource_group {}
+variable location {}
 # Network
-variable virtual_network_name {
-    default = "udacity-network"
-}
-variable address_space {
-    default = ["10.5.0.0/16"]
-}
-variable "application_type" {
-    default = "myApplication"
-}
-variable "resource_type" {
-    default = "NET"
-}
-variable "address_prefix_test" {
-    default = "10.5.1.0/24"
-}
-
+variable virtual_network_name {}
+variable address_space {}
+variable "application_type" {}
+variable "resource_type" {}
+variable "address_prefix_test" {}
