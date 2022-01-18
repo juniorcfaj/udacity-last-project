b79d141aa4917df6b066b8b5f518cc14711d721e
juniorcfaj <junior.cfaj@gmail.com>
2022-01-18 20:57:00 -0300

    agora

--- terraform/input.tf
+++ terraform/input.tf
@@ -22,9 +22,11 @@ variable "virtual_network_name" {
   default = "udacity-network"
 }
 variable "address_prefix_test" {
-    default = "10.5.1.0/24"
+  default = "10.5.1.0/24"
 }
-variable "address_space" {}
+variable "address_space" {
+  default = ["10.5.0.0/16"]
+}
 
 # VM
 variable "admin_username" {
