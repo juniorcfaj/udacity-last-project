e9d6ad42a9a0a6f9bfd0bec0f2bba876d4460731
juniorcfaj <junior.cfaj@gmail.com>
2022-01-18 21:34:14 -0300

    aaaaa

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -7,7 +7,7 @@ resource "azurerm_network_interface" "test" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = var.public_ip
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
